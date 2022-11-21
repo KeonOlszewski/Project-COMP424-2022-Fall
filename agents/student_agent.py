11a9f2c0631e7862f46dcb4b04586a6e776ca798
gtjarvis <31189984+gtjarvis@users.noreply.github.com>
2022-11-21 16:29:50 -0500

    Functions to get list of valid moves

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -14,6 +14,7 @@ class StudentAgent(Agent):
     def __init__(self):
         super(StudentAgent, self).__init__()
         self.name = "StudentAgent"
+        self.autoplay = True
         self.dir_map = {
             "u": 0,
             "r": 1,
@@ -22,19 +23,47 @@ class StudentAgent(Agent):
         }
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
-        """
-        Implement the step function of your agent here.
-        You can use the following variables to access the chess board:
-        - chess_board: a numpy array of shape (x_max, y_max, 4)
-        - my_pos: a tuple of (x, y)
-        - adv_pos: a tuple of (x, y)
-        - max_step: an integer
+        valid_moves = self.get_all_moves(chess_board, my_pos, adv_pos, max_step)
+        chosen_move = valid_moves[-1] #TODO make an algorithm that picks moves better
+        return (chosen_move[0],chosen_move[1]), chosen_move[2]
 
-        You should return a tuple of ((x, y), dir),
-        where (x, y) is the next position of your agent and dir is the direction of the wall
-        you want to put on.
-
-        Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
-        """
-        # dummy return
-        return my_pos, self.dir_map["u"]
+    def get_moves_from_position(self, chess_board, pos, adv_pos, max_step, moves, steps):
+        #Up, Right, Down, Left
+        #(-1, 0), (0, 1), (1, 0), (0, -1)
+        x = pos[0]
+        y = pos[1]
+        steps = pos[2]
+        new_moves = []
+        if steps + 1 > max_step:
+            return []
+        if not chess_board[x][y][self.dir_map["u"]] and (x-1, y) not in moves and not (x-1, y) == adv_pos:
+            new_moves.append((x-1, y, steps+1))
+        if not chess_board[x][y][self.dir_map["r"]] and (x, y+1) not in moves and not (x, y+1) == adv_pos:
+            new_moves.append((x, y+1, steps+1))
+        if not chess_board[x][y][self.dir_map["d"]] and (x+1, y) not in moves and not (x+1, y) == adv_pos:
+            new_moves.append((x+1, y, steps+1))
+        if not chess_board[x][y][self.dir_map["l"]] and (x, y-1) not in moves and not (x, y-1) == adv_pos:
+            new_moves.append((x, y-1, steps+1))
+        return new_moves
+    
+    def get_all_moves(self, chess_board, og_pos, adv_pos, max_step):
+        moves = [og_pos]
+        new_moves = [(og_pos[0],og_pos[1],0)] #starting position with 0 steps so far
+        while len(new_moves) > 0:
+            move = new_moves[0]
+            new_moves = new_moves[1:]
+            new = self.get_moves_from_position(chess_board, move, adv_pos, max_step, moves, new_moves)
+            for move in new:
+                moves.append((move[0], move[1]))
+            new_moves += new
+        moves_with_barrier = []
+        for move in moves:
+            if not chess_board[move[0]][move[1]][self.dir_map["u"]]:
+                moves_with_barrier.append((move[0],move[1],self.dir_map["u"]))
+            if not chess_board[move[0]][move[1]][self.dir_map["d"]]:
+                moves_with_barrier.append((move[0],move[1],self.dir_map["d"]))
+            if not chess_board[move[0]][move[1]][self.dir_map["l"]]:
+                moves_with_barrier.append((move[0],move[1],self.dir_map["l"]))
+            if not chess_board[move[0]][move[1]][self.dir_map["r"]]:
+                moves_with_barrier.append((move[0],move[1],self.dir_map["r"]))
+        return moves_with_barrier

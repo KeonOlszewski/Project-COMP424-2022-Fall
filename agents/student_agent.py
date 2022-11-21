b293b9d619b70c4bb43dc4592cff1437391b85d5
gtjarvis <31189984+gtjarvis@users.noreply.github.com>
2022-11-21 17:30:56 -0500

    added comments and made get_best_move faster

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -24,22 +24,39 @@ class StudentAgent(Agent):
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
         valid_moves = self.get_valid_moves(chess_board, my_pos, adv_pos, max_step)
-        chosen_move =  self.get_best_move(chess_board, valid_moves)
+        chosen_move =  self.get_best_move(chess_board, valid_moves, my_pos, adv_pos,)
         return (chosen_move[0],chosen_move[1]), chosen_move[2]
 
-    #Improve best move algorithm
-    def get_best_move(self, chess_board, valid_moves):
+    '''
+    Currently for each valid moves gets the score of the board if that move was made.
+    If that move keeps the score the same as current score, play that move.
+    Otherwise, append it to the score list and check next move.
+    '''
+    def get_best_move(self, chess_board, valid_moves, my_pos, adv_pos):
         scores = []
+        cur_score = self.get_score(chess_board, my_pos)
         for i in range(len(valid_moves)):
             move = valid_moves[i]
             chess_board[move[0], move[1], move[2]] = True
-            scores.append(self.get_score(chess_board, (move[0],move[1])))
+            score = self.get_score(chess_board, (move[0],move[1]))
             chess_board[move[0], move[1], move[2]] = False
+            if score == cur_score:
+                return valid_moves[i]
+            scores.append(score)
         return valid_moves[scores.index(max(scores))]
 
+    '''
+    Score is identical to number of reachable positions given a max step size 2*board_size (full board).
+    Need to set the adv position to your position so the adversary doesn't block any positions.
+    '''
     def get_score(self, chess_board, pos):
         return len(self.get_valid_positions(chess_board, pos, pos, 2*len(chess_board)))
 
+    '''
+    Given a position on the board, returns a list of adjacent open positions.
+    Takes into account max step size, adv position, barriers and the current valid moves list 
+    (so as not to include duplicates)
+    '''
     def get_moves_from_position(self, chess_board, pos, adv_pos, max_step, moves, steps):
         #Up, Right, Down, Left
         #(-1, 0), (0, 1), (1, 0), (0, -1)
@@ -59,6 +76,10 @@ class StudentAgent(Agent):
             new_moves.append((x, y-1, steps+1))
         return new_moves
 
+    '''
+    Continuously calls the get_moves_from_position function on each new position found until all
+    reachable positions have been found
+    '''
     def get_valid_positions(self, chess_board, og_pos, adv_pos, max_step):
         moves = [og_pos]
         new_moves = [(og_pos[0],og_pos[1],0)] #starting position with 0 steps so far
@@ -71,6 +92,11 @@ class StudentAgent(Agent):
             new_moves += new
         return moves
     
+    '''
+    Uses get_valid_positions to get a list of all reachable positions.
+    Then determines from each position what barriers can be placed and creates a list of all
+    position/barrier combinations that make up a move.
+    '''
     def get_valid_moves(self, chess_board, og_pos, adv_pos, max_step):
         moves = self.get_valid_positions(chess_board, og_pos, adv_pos, max_step)
         moves_with_barrier = []
